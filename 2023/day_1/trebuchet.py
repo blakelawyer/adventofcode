cbe5cbac4872e1e4ec2357ff1fad1c123100a377
Blake Lawyer <blakethelawyer@gmail.com>
2023-12-02 17:04:12 +0000

    day 1 part 2

--- 2023/day_1/trebuchet.py
+++ 2023/day_1/trebuchet.py
@@ -1,4 +1,3 @@
-# Part 1
 file = open("./trebuchet.txt", "r")
 lines = [line.strip() for line in file]
 
@@ -10,3 +9,23 @@ for line in lines:
             digits = digits + c
     if digits != 0:
         total += int(digits[0] + digits[-1])
+print(f"Part 1: {total}")
+
+def digitize(line, numbers):
+    for i, num in enumerate(numbers):
+        line = line.replace(num, num[0] + str(i+1) + num[-1])
+    return line
+
+total = 0
+numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+for line in lines:
+    line = digitize(line, numbers)
+    digits = ""
+    for c in line:
+        if c.isdigit():
+            digits = digits + c
+    if digits != 0:
+        combination = int(digits[0] + digits[-1])
+        total += combination 
+print(f"Part 2: {total}")
+
